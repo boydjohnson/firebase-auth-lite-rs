c8d3177e35e58b206927cd1a0dcc9704d13faf02
Boyd Johnson <johnson.boyd@gmail.com>
2022-04-16 22:42:35 +0000

    WIP

---
+++ examples/yew-email-password-example/src/home.rs
@@ -0,0 +1,59 @@
+use crate::{Route, API_KEY};
+use firebase_auth_lite::{Auth, AuthOptions};
+use yew::prelude::*;
+use yew_router::components::Link;
+
+pub struct Home {
+    user: Option<String>,
+}
+
+pub enum HomeMsg {
+    NotLoggedIn,
+    LoggedIn(String),
+}
+
+impl Component for Home {
+    type Message = HomeMsg;
+    type Properties = ();
+
+    fn create(ctx: &Context<Self>) -> Self {
+        let auth = Auth::new(AuthOptions::new(API_KEY, "".into()));
+
+        ctx.link().send_future(async move {
+            match auth.fetch_profile().await {
+                Ok(prof) => HomeMsg::LoggedIn("User".into()),
+                Err(_) => HomeMsg::NotLoggedIn,
+            }
+        });
+
+        Home { user: None }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            HomeMsg::NotLoggedIn => {
+                self.user = None;
+            }
+            HomeMsg::LoggedIn(user) => {
+                self.user = Some(user);
+            }
+        }
+        true
+    }
+
+    fn view(&self, _ctx: &Context<Self>) -> Html {
+        if let Some(user) = &self.user {
+            html! {
+                <h1> { format!("Hello, {}", user) }</h1>
+            }
+        } else {
+            html! {
+                <>
+                <h1>{ "Please Sign up or Log in" }</h1>
+                <span><Link<Route> to = { Route::Signup }>{ "Sign Up" }</Link<Route>></span>
+                <span><Link<Route> to = { Route::Login }>{ "Log In" }</Link<Route>></span>
+                </>
+            }
+        }
+    }
+}
--- examples/yew-email-password-example/src/signup.rs
+++ examples/yew-email-password-example/src/signup.rs
@@ -1,1 +1,113 @@
+use crate::{Route, API_KEY};
+use firebase_auth_lite::{Auth, AuthOptions};
+use yew::{events::InputEvent, prelude::*};
+use yew_router::{history::History, hooks::use_history};
 
+pub struct Signup {
+    email: Option<String>,
+    password: Option<String>,
+    signup_in_progress: bool,
+}
+
+pub enum SignupMsg {
+    EmailInput(String),
+    PasswordInput(String),
+    ChangeToHome,
+    Signup,
+    Nil,
+}
+
+impl Component for Signup {
+    type Message = SignupMsg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Signup {
+            email: None,
+            password: None,
+            signup_in_progress: false,
+        }
+    }
+
+    fn update(&mut self, ctx: &Context<Self>, msg: SignupMsg) -> bool {
+        match msg {
+            SignupMsg::EmailInput(email) => {
+                self.email = Some(email);
+                false
+            }
+            SignupMsg::PasswordInput(password) => {
+                self.password = Some(password);
+                false
+            }
+            SignupMsg::Signup => {
+                let auth = Auth::new(AuthOptions::new(API_KEY, "".into()));
+                if let (Some(email), Some(password)) = (&self.email, &self.password) {
+                    let email = email.clone();
+                    let password = password.clone();
+                    ctx.link().send_future(async move {
+                        if auth.signUp(email, password).await.is_ok() {
+                            SignupMsg::ChangeToHome
+                        } else {
+                            SignupMsg::Nil
+                        }
+                    });
+
+                    self.signup_in_progress = true;
+
+                    true
+                } else {
+                    false
+                }
+            }
+            SignupMsg::ChangeToHome => {
+                if let Some(history) = use_history() {
+                    history.push(Route::Home);
+                }
+                true
+            }
+            SignupMsg::Nil => false,
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let on_input_email = ctx.link().callback(|evt: InputEvent| {
+            if let Some(email) = evt.data() {
+                SignupMsg::EmailInput(email)
+            } else {
+                SignupMsg::Nil
+            }
+        });
+
+        let on_input_password = ctx.link().callback(|evt: InputEvent| {
+            if let Some(password) = evt.data() {
+                SignupMsg::PasswordInput(password)
+            } else {
+                SignupMsg::Nil
+            }
+        });
+
+        let on_click_signup = ctx.link().callback(|_| SignupMsg::Signup);
+
+        if self.signup_in_progress {
+            html! {
+                <p> { "Working on sign up" }</p>
+            }
+        } else {
+            html! {
+                <>
+                <div>
+                    <label>{ "Email" }
+                        <input type = "email" oninput = {on_input_email} name="email" />
+                    </label>
+                </div>
+                <div>
+                    <label>{ "Password" }
+                        <input type = "password" oninput = {on_input_password} name="password" />
+                    </label>
+                </div>
+                <button onclick = {on_click_signup}>{ "Signup" }</button>
+                </>
+            }
+        }
+    }
+}
